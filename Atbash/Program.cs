e5a0b6244151ccb646076b395981ca3b9772e35e
4doner4 <damir.ametov34@gmail.com>
2022-09-12 11:15:50 +0600

    Atbash

---
+++ Atbash/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+string str = "abc";
+
+Console.WriteLine(Encrypt(str));
+
+string Encrypt(string word)
+{
+    StringBuilder sbResult = new StringBuilder();
+
+    for (int i = 0; i < word.Length; i++)
+    {
+        sbResult.Append((char)((int)'z' - ((int)word[i] - (int)'a')));
+    }
+
+    return sbResult.ToString();
+}
